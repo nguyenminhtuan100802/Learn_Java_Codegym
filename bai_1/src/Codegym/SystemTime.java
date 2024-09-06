c6d55da027d855d5033398fd970621f54c507208
nguyenminhtuan100802 <139602897+nguyenminhtuan100802@users.noreply.github.com>
2024-09-06 11:23:21 +0700

    6/9/24 - Thực hành hiển thị thời gian

---
+++ bai_1/src/Codegym/SystemTime.java
@@ -0,0 +1,10 @@
+package Codegym;
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
