4ee81a9e8f6d1f41b6fb9299ba9dfed1e57367f5
nguyenminhtuan100802 <139602897+nguyenminhtuan100802@users.noreply.github.com>
2024-09-11 10:32:24 +0700

    11/9/24 - Practice create  a fan class

---
+++ bai_1/src/Codegym/ClassObject/Fan/Main.java
@@ -0,0 +1,8 @@
+package Codegym.ClassObject.Fan;
+
+public class Main {
+    public static void main(String[] args) {
+        Fan fan1 = new Fan(3, true, 10, "yellow");
+        Fan fan2 = new Fan(2, true, 5, "yellow");
+    }
+}
